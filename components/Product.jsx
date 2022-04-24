d86ac8f28f5e1d751ced4b1cfedfc81fc24ba9dd
quantumDev <tests8q@gmail.com>
2022-04-24 16:47:23 +0200

    rédaction de la page produit avec carroussel de miniatures produits et galerie produit

--- components/Product.jsx
+++ components/Product.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
+import Link from 'next/link';
 
-import Link from "next/link";
-
-import { urlFor } from "../lib/client";
+import { urlFor } from '../lib/client';
 
 const Product = ({ product: { image, name, slug, price } }) => {
   return (
@@ -10,17 +9,17 @@ const Product = ({ product: { image, name, slug, price } }) => {
       <Link href={`/product/${slug.current}`}>
         <div className="product-card">
           <img 
-          src={urlFor(image && image[0])}
-          width={250}
-          height={250}
-          className="product-image"
-           />
-           <p className="product-name">{name}</p>
-           <p className="product-price">{price}€</p>
+            src={urlFor(image && image[0])}
+            width={250}
+            height={250}
+            className="product-image"
+          />
+          <p className="product-name">{name}</p>
+          <p className="product-price">${price}</p>
         </div>
       </Link>
     </div>
-  );
-};
+  )
+}
 
-export default Product;
+export default Product
